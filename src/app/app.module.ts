c7f8b13afcd04bed3e1f8a771272f47d587b3c32
LOZANO DOLZ  RAMON <a044525499y@stretch-builder>
2019-01-16 20:02:55 +0100

    comienzo proyecto cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './header-component/header-component.component';
+import { LoginComponentComponent } from './header-component/login-component/login-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponentComponent,
+    LoginComponentComponent
   ],
   imports: [
     BrowserModule,
